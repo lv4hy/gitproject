f36332f8785e9253775536f2392ffc034962ab39
lv4hy <357204564@qq.com>
2017-05-23 10:20:28 +0800

    gitproject first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ctis-szx on 2017/5/23.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("this is the first class");
+    }
+}
